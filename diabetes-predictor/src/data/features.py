950bfdb47381e333bf9351246aeb8ab87559685f
Jason Parker <jason.parker@hey.com>
2021-10-05 21:21:33 -0400

    Create features.py

---
+++ diabetes-predictor/src/data/features.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def create_hyperglyemic_flag(series, **kwargs):
+    return np.where(
+        series > (kwargs.get('glucose_value') or 240), 1, 0
+    )
+
+def create_hypoglyemic_flag(series, **kwargs):
+    return np.where(
+        series < (kwargs.get('glucose_value') or 100), 1, 0
+    )
+
+def generate_features(df):
+    result = df.copy(deep=True)
+    result['hyperglycemic_flag'] = create_hyperglyemic_flag(result['Glucose'])
+    result['hypoglycemic_flag'] = create_hypoglyemic_flag(result['Glucose'])
+    return result
